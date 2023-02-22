59d6940493bec2105f122d82df85ce53da8f08e6
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-22 22:45:58 +0700

    Java 9 | reactiveStream

---
+++ Java9/src/com/ngochung/ReactiveStreamsExample.java
@@ -0,0 +1,11 @@
+package com.ngochung;
+
+public class ReactiveStreamsExample {
+//	public static void main(String[] args) {
+//		// Tạo một Publisher
+//		Flux<Integer> flux = Flux.range(1, 10).filter(n -> n % 2 == 0).publishOn(Schedulers.parallel());
+//
+//		// Tạo một Subscriber
+//		flux.subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Done"));
+//	}
+}
