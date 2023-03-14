0d63789e0ee54a87e4160e15c88867e289824ab8
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-03-14 21:17:27 +0700

    java8 | practice with optional

---
+++ Java8/src/com/ngochung/model/Class.java
@@ -0,0 +1,23 @@
+package com.ngochung.model;
+
+public class Class {
+	private String name;
+	private int totalMember;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalMember() {
+		return totalMember;
+	}
+
+	public void setTotalMember(int totalMember) {
+		this.totalMember = totalMember;
+	}
+
+}
